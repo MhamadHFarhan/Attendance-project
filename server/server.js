52e03720e48e8179c1cf861a645aebb1e35fa504
Mohamad-Farhan <mohammednm40@gmail.com>
2021-10-10 18:16:15 +0300

    Install the packeges need it and make the starter setup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// middlewaers
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(cors());
+
+// routes middleware
+
+const port = process.env.PORT
